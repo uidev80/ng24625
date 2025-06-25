bc3780abeb647b269090d5c294320272d79b4c86
uidev80 <uidev80@gmail.com>
2025-06-25 20:17:00 +0530

    'ngbuild'

--- src/app/counter-service.spec.ts
+++ src/app/counter-service.spec.ts
@@ -2,6 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { CounterService } from './counter-service';
 
+/**
+ * Unit tests for {@link CounterService}.
+ *
+ * This test suite verifies:
+ * - Service creation
+ * - Initial counter value
+ * - Increment and decrement functionality
+ * - Emission of new values via the observable stream
+ */
 describe('CounterService', () => {
   let service: CounterService;
 
@@ -12,5 +21,39 @@ describe('CounterService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have initial value 0', () => {
+    expect(service.value).toBe(0);
+  });
+
+  it('should increment the counter', () => {
+    service.increment();
+    expect(service.value).toBe(1);
+    service.increment();
+    expect(service.value).toBe(2);
+  });
+
+  it('should decrement the counter', () => {
+    service.increment();
+    service.increment();
+    service.decrement();
+    expect(service.value).toBe(1);
+    service.decrement();
+    expect(service.value).toBe(0);
+  });
+
+  it('should emit new values via counter$', (done) => {
+    const emitted: number[] = [];
+    const sub = service.counter$.subscribe((val) => {
+      emitted.push(val);
+      if (emitted.length === 3) {
+        expect(emitted).toEqual([0, 1, 2]);
+        sub.unsubscribe();
+        done();
+      }
+    });
+    service.increment();
+    service.increment();
   });
 });
--- src/app/counter-service.ts
+++ src/app/counter-service.ts
@@ -1,31 +1,41 @@
-// import { Injectable } from '@angular/core';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class CounterService {
-
-//   constructor() { }
-// }
-
-// counter.service.ts
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+/**
+ * Service for managing a simple counter state using RxJS BehaviorSubject.
+ * Provides observable stream for counter value and methods to increment/decrement.
+ */
 @Injectable({ providedIn: 'root' })
 export class CounterService {
+  /**
+   * Internal BehaviorSubject holding the current counter value.
+   * @private
+   */
   private counter = new BehaviorSubject<number>(0);
+
+  /**
+   * Observable stream of the current counter value.
+   */
   counter$ = this.counter.asObservable();
 
+  /**
+   * Increments the counter value by 1.
+   */
   increment() {
     this.counter.next(this.counter.value + 1);
   }
 
+  /**
+   * Decrements the counter value by 1.
+   */
   decrement() {
     this.counter.next(this.counter.value - 1);
   }
 
-  // get value() {
-  //   return this.counter.value;
-  // }
+  /**
+   * Gets the current value of the counter.
+   */
+  get value() {
+    return this.counter.value;
+  }
 }
